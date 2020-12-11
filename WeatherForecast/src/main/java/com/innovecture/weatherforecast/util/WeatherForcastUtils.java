0862954350f0d2a725a0c27b9b69bb5839333e9f
Ziauddin135 <zia.uddin135@gmail.com>
2020-12-11 12:12:15 +0530

    Add files via upload
    committed utility class

---
+++ WeatherForecast/src/main/java/com/innovecture/weatherforecast/util/WeatherForcastUtils.java
@@ -0,0 +1,54 @@
+package com.innovecture.weatherforecast.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+/**
+ * @author zia.shaikh
+ *
+ */
+public class WeatherForcastUtils {
+
+	private static final String key = "aesEncryptionKey";
+	private static final String initVector = "encryptionIntVec";
+	/** Method commented to garbled the string**/
+	/*public static String encrypt(String value) {
+		try {
+			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+			SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+			byte[] encrypted = cipher.doFinal(value.getBytes());
+			return Base64.encodeBase64String(encrypted);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}*/
+
+	public static String decrypt(String encrypted) {
+		String ungarbledString = "";
+		try {
+			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+			SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+			byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+			ungarbledString = new String(original);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return ungarbledString;
+	}
+
+	
+
+}
