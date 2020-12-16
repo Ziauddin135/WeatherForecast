033ebd1e66587c6154fa839a4bf5e0155e7af24d
Sheeraz <er.syedsheerazshaukat@gmail.com>
2020-12-16 20:42:16 +0530

    Commit Controller Layer

--- WeatherForecast/src/main/java/com/innovecture/weatherforecast/WeatherForecastApplication.java
+++ WeatherForecast/src/main/java/com/innovecture/weatherforecast/WeatherForecastApplication.java
@@ -7,26 +7,28 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Profile;
 
 import com.innovecture.weatherforecast.service.IWeatherForecastCalculatorService;
 import com.innovecture.weatherforecast.util.WeatherForecastConstants;
 
 @SpringBootApplication
-public class WeatherForecastApplication implements ApplicationRunner {
+@ComponentScan("com.innovecture.weatherforecast")
+public class WeatherForecastApplication /*implements ApplicationRunner*/ {
 
 	private static final int ARG_INDEX = 0;
 
 	private static final Logger logger = LoggerFactory.getLogger(WeatherForecastApplication.class);
 
-	@Autowired
-	IWeatherForecastCalculatorService weatherForecastCalculatorService;
+	/*@Autowired
+	IWeatherForecastCalculatorService weatherForecastCalculatorService;*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherForecastApplication.class, args);
 	}
 
-	@Override
+/*	@Override
 	public void run(ApplicationArguments args) throws Exception {
 
 		Integer zipcode = Integer.parseInt(args.getOptionValues(WeatherForecastConstants.ZIPCODE).get(ARG_INDEX));
@@ -34,5 +36,5 @@ public class WeatherForecastApplication implements ApplicationRunner {
 		String consoleOutput = weatherForecastCalculatorService.getMinimumForcastForNthDay(zipcode, dayOfForecast);
 		logger.info("output==>" + consoleOutput);
 	}
-
+*/
 }
