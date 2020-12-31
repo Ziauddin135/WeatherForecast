30ced1357122099ff76aa6ad4168948492f5fd54
Ziauddin135 <zia.uddin135@gmail.com>
2020-12-31 11:54:36 +0530

    Add files via upload
    added Request params to calculate weather forcast

--- WeatherForecast/src/main/java/com/innovecture/weatherforecast/controller/WeatherForecastController.java
+++ WeatherForecast/src/main/java/com/innovecture/weatherforecast/controller/WeatherForecastController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.innovecture.weatherforecast.exception.ResourceNotFoundException;
@@ -24,9 +24,9 @@ public class WeatherForecastController {
 	
 	
 	
-	@GetMapping("calculator/{zipcode}/{dayOfForecast}")
-	public  ResponseEntity<String>  getForeCastDetails(@PathVariable("zipcode")	Integer zipcode
-			, @PathVariable("dayOfForecast") Integer dayOfForecast) throws ResourceNotFoundException , WeatherForecastServiceException {
+	@GetMapping("calculator")
+	public  ResponseEntity<String>  getForeCastDetails(@RequestParam(name = "zip") Integer zipcode
+			, @RequestParam(name = "day") Integer dayOfForecast) throws ResourceNotFoundException , WeatherForecastServiceException {
 		String response = "";
 		
 		
